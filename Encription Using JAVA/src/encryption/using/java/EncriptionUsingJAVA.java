50fcab5cc73490c34caef210ff51342c1e077309
Aniket Gaikwad <100527275+aniketkumargaikwad@users.noreply.github.com>
2022-03-19 16:27:39 +0530

    Add files via upload

---
+++ Encription Using JAVA/src/encryption/using/java/EncriptionUsingJAVA.java
@@ -0,0 +1,13 @@
+
+package encryption.using.java;
+
+
+public class EncriptionUsingJAVA {
+
+
+    public static void main(String[] args) {
+        
+        EncryptionProgram ep= new EncryptionProgram();
+    }
+    
+}
